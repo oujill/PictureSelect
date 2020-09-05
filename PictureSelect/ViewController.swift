b805db4ad0235f675a52b5c5e0fb1a8b2e902af6
jillou <jilljill860524@gmail.com>
2020-09-05 13:49:06 +0800

    can choose/take a picture from your phone

--- PictureSelect/ViewController.swift
+++ PictureSelect/ViewController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func choosepic(_ sender: UIButton) {
+        let controller = UIImagePickerController()
+        //controller.sourceType = .photoLibrary//選照片功能用這個
+        controller.sourceType = .camera//照相功能用這個
+        controller.delegate = self
+        present(controller, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.originalImage] as! UIImage
+        imageView.image = image
+        dismiss(animated: true, completion: nil)
+    }
 }
 
